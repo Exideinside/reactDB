b094dee7c028ebcecc6c384ddfcdfda4e11da0bc
Exideinside <70756794+Exideinside@users.noreply.github.com>
2021-05-08 19:32:55 +0300

    Add files via upload

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -3,10 +3,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { createStyles, fade, Theme, makeStyles } from '@material-ui/core/styles';
+import { createStyles, fade, Theme, makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+import green from '@material-ui/core/colors/green';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,29 +21,6 @@ const useStyles = makeStyles((theme: Theme) =>
       [theme.breakpoints.up('sm')]: {
         display: 'block',
       },
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: fade(theme.palette.common.white, 0.15),
-      '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
-      },
-      marginLeft: 0,
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-      },
-    },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
     },
     inputRoot: {
       color: 'inherit',
@@ -65,10 +41,19 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+const theme = createMuiTheme({
+  palette:{
+    primary:{
+      main: green[600],
+    }
+  }
+});
+
 export default function SearchAppBar() {
   const classes = useStyles();
-
+  const colors = useStyles();
   return (
+    <ThemeProvider theme={theme}>
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
@@ -83,21 +68,9 @@ export default function SearchAppBar() {
           <Typography className={classes.title} variant="h6" noWrap>
             Таблица преподавателей
           </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Поиск..."
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </div>
         </Toolbar>
       </AppBar>
     </div>
+    </ThemeProvider>
   );
 }
